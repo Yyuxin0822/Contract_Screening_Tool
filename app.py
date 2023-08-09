d298a6264b90e528fce7e903a83f30ee4fedf4ba
Ubuntu <yyuxin0822@gmail.com>
2023-08-09 20:16:47 +0000

    upload files

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+from waitress import serve
+from form import UploadFileForm
+from werkzeug.utils import secure_filename
+import os
+from jinja2 import BaseLoader, Markup, TemplateNotFound, nodes
+
+
+app=Flask(__name__)
+app.config['SECRET_KEY']="secretkey"
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    form=UploadFileForm()
+
+    if form.validate_on_submit():
+        file=form.file.data 
+        file.save(os.path.join
+                  (os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+        return "File Uploaded Successfully."
+    return render_template("index.html", form=form)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8080, threaded=True)
+    #serve(app, host='0.0.0.0', port=8080, url_scheme="https")
---
+++ form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, RadioField, validators
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.validators import DataRequired
+
+class UploadFileForm(FlaskForm):
+    file = FileField('File', validators=[FileRequired(), FileAllowed(['docx'], 'docx only!')])
+    submit = SubmitField('Upload File')
+    #restrict file to .docx
+    #file = FileField('File', validators=[FileRequired(), FileAllowed(['docx'], 'docx only!')])
